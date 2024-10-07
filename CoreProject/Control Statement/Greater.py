86b70d9e9a22a11ac7cd481144e21a975ab3efa1
VIshal6620 <vishalvish7099@gmail.com>
2024-10-07 11:55:25 +0530

    oky

--- CoreProject/Control Statement/Greater.py
+++ CoreProject/Control Statement/Greater.py
@@ -2,4 +2,3 @@ a = 400
 b = 656
 if a * b:
     print("a is greater than b")
-
--- CoreProject/Control Statement/Test Continue Statment.py
+++ CoreProject/Control Statement/Test Continue Statment.py
@@ -1,5 +1,4 @@
-i = 1;
-for i in range(1,10):
+for i in range(1, 11):
     if i == 5:
-        continue;
-print("%d" % i);
+        continue  # Skip the number 5
+    print(i)  # Print the current number
