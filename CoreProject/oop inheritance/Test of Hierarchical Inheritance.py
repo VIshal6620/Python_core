ff67da729129d07a7727638c3e5de2ac562d2fd3
VIshal6620 <vishalvish7099@gmail.com>
2024-10-23 12:22:36 +0530

    oky

--- CoreProject/oop inheritance/Test of Hierarchical Inheritance.py
+++ CoreProject/oop inheritance/Test of Hierarchical Inheritance.py
@@ -1,18 +1,43 @@
-class Parent:
-    def show(self):
-        print("parent method called")
+# class Parent:
+#     def show(self):
+#         print("parent method called")
+#
+# class Child1(Parent):
+#     def display1(self):
+#         print("Child1 method called")
+#
+# class Child2(Parent):
+#     def display2(self):
+#         print("Child2 method called")
+#
+# obj1=Child1()
+# obj2=Child2()
+# obj1.display1()
+# obj1.show()
+# obj2.display2()
+# obj2.show()
+#
+class India():
+    def capital(self):
+        print("new delhi")
 
-class Child1(Parent):
-    def display1(self):
-        print("Child1 method called")
+    def language(self):
+        print("Hindu the primary")
 
-class Child2(Parent):
-    def display2(self):
-        print("Child2 method called")
+    def type(self):
+        print("india developing")
 
-obj1=Child1()
-obj2=Child2()
-obj1.display1()
-obj1.show()
-obj2.display2()
-obj2.show()
+
+class USA():
+    def capital(self):
+        print("Washington,D.C is the capital")
+
+    def language(self):
+        print("English is the primary")
+
+
+obj_ind = India()
+obj_usa = USA()
+for country in (obj_ind, obj_usa):
+    country.capital()
+    country.language()
