d955c886272077dc8a9b4d8742a025eacf01c8cf
VIshal6620 <vishalvish7099@gmail.com>
2024-11-07 13:06:31 +0530

    oky

---
+++ CoreProject/Arrays/test of arrys collection.py
@@ -0,0 +1,38 @@
+# from array import array
+#
+# array1 = array('i', [10, 20, 30, 40, 50])
+# print(array1[0])
+# print(array1[2])
+from tkinter.font import names
+
+# Access the Elements of an Array
+
+# names=["Ram","Shyam","Raju"]
+# x=names[0];
+# print(x)
+
+# The length of an Array
+# names=["Ram","shyam","Raju"]
+# print(len(names))
+
+# #insertion operation
+# from array import array
+# array1=array('i',[10,20,30,40,50,60])
+# array1.insert(1,80)
+# for x in array1:
+#     print(x)
+
+# Delete operation
+# from array import array
+# array1 = array('i',[10,20,30,40,50,60,70,80])
+# array1.remove(60)
+# for x in array1:
+#     print(x)
+
+# update operation
+from array import array
+
+array1 = array('i', [10, 20, 30, 40, 50])
+array1[2] = 656
+for x in array1:
+    print(x)
