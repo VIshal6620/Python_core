98a139f39817ff9069be98d2b42c3e4fff878f26
VIshal6620 <vishalvish7099@gmail.com>
2025-06-30 14:15:09 +0530

    30-06-2025

--- CoreProject/Test Function/new function.py
+++ CoreProject/Test Function/new function.py
@@ -1,10 +1,10 @@
-# def vishal():
-#     print("This is my first function")
-#
-# vishal()
-#
-#
-#
-# country_name = input("enter country Name" : "")
-# def my_function(country =country_name):
-#     print('i am from'+country)
+def vishal():
+    print("This is my first function")
+
+vishal()
+
+
+
+country_name = input("enter country Name"  "")
+def my_function(country =country_name):
+    print('i am from'+country)
