d452a5b8028d354c4a4bd0a6a4bb5ef7e17445a2
VIshal6620 <vishalvish7099@gmail.com>
2025-07-25 15:53:15 +0530

    25-074-2025

--- CoreProject/Baisc/Add.py
+++ CoreProject/Baisc/Add.py
@@ -1,53 +1,7 @@
-# a = 5
-# b = 9
-# c = a+b
-#
-# print("addition",c)
-# print(type(a))
-#
-#
-#
-#
-#
-# import datetime
-#
-# today = datetime.date.today()
-# future = today + datetime.timedelta(days=7)
-# past = today - datetime.timedelta(days=7)
-#
-# print("7 days later:", future)
-# print("7 days ago:", past)
-#
-#
-#
-#
-#
-# import datetime
-#
-# today = datetime.date.today()
-# future = today + datetime.timedelta(days=7)
-# past = today - datetime.timedelta(days=7)
-#
-# print('future dayes of 7',future)
-# print('past of 7 days',past)
+a = 5
+b = 9
+c = a+b
 
-class Employee:
-    'I am Employee'
+print("addition",c)
+print(type(a))
 
-    def __init__(self, salary):
-        self.salary = salary
-
-    def set_salary(self, salary):
-        self.salary = salary
-
-    def get_salary(self):
-        return self.salary
-
-
-emp = Employee(2000)
-
-print(emp)
-
-print(id(emp))
-
-print(emp.__doc__)
