8d994664d9d5c934cd8011c651e7abb9cb07a23f
VIshal6620 <vishalvish7099@gmail.com>
2025-08-12 17:08:12 +0530

    12-08-2025

--- CoreProject/Class and Object/Person.py
+++ CoreProject/Class and Object/Person.py
@@ -1,47 +1,47 @@
-# class Person:
-#
-#     def __init__(self, name, salary, city):
-#         self.name = name
-#         self.salary = salary
-#         self.city = city
-#
-#     def getname(self):
-#         return self.name
-#
-#     def getsalary(self):
-#         return self.salary
-#
-#     def getcity(self):
-#         return self.city
-#
-#
-# p = Person("vishal", "10000000", "Dewas")
-# print(p.getname(), p.getsalary(), p.getcity())
-
-
-
 class Person:
-    def __init__(self,name=None,age=None):
-        self.name = name
-        self.age = age
 
-    def setname(self,name):
+    def __init__(self, name, salary, city):
         self.name = name
+        self.salary = salary
+        self.city = city
 
     def getname(self):
         return self.name
 
-    def setage(self, age):
-        self.age = age
+    def getsalary(self):
+        return self.salary
 
-    def getage(self):
-        return self.age
+    def getcity(self):
+        return self.city
 
-p = Person()
-p.setname('jatin')
-print(p.getname())
 
-p.setage(85)
-print(p.getage())
+p = Person("vishal", "10000000", "Dewas")
+print(p.getname(), p.getsalary(), p.getcity())
+
+
+
+# class Person:
+#     def __init__(self,name=None,age=None):
+#         self.name = name
+#         self.age = age
+#
+#     def setname(self,name):
+#         self.name = name
+#
+#     def getname(self):
+#         return self.name
+#
+#     def setage(self, age):
+#         self.age = age
+#
+#     def getage(self):
+#         return self.age
+#
+# p = Person()
+# p.setname('jatin')
+# print(p.getname())
+#
+# p.setage(85)
+# print(p.getage())
 
 
