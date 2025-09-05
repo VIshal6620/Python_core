87705ce2520b5cfa16c7febb9a22c1cd708e9e98
VIshal6620 <vishalvish7099@gmail.com>
2025-09-05 17:32:47 +0530

    05-09-2025

--- CoreProject/Thread/inherit threads.py
+++ CoreProject/Thread/inherit threads.py
@@ -1,11 +1,49 @@
-from threading import Thread
+from time import sleep
+from threading import *
+import threading
 
 
-class hi(Thread):
+class Account:
+
+    def __init__(self):
+        self.balance = 0
+
+    def set_balance(self, balance):
+        sleep(1)
+        self.balance = balance
+
+    def get_balance(self):
+        sleep(1)
+        return self.balance
+
+    def diposite(self, amount):
+        bal = self.get_balance()
+        self.set_balance(bal + amount)
+
+
+class Racing(Thread):
+
+    def __init__(self, account: Account, name):
+        super().__init__()
+        self.account = account
+        self.name = name
+
     def run(self):
-        for i in range(5):
-            print("hi", i)
+        for i in range(10):
+            self.account.diposite(100)
+            print(self.name, self.account.get_balance())
 
 
-t1 = hi()
-t1.start()
+def main_task():
+    acc = Account()
+    # create thread instances
+    t1 = Racing(acc, "abc")
+    t2 = Racing(acc, "xyz")
+
+    # Start threads
+    t1.start()
+    t2.start()
+
+
+# Run main task
+main_task()
