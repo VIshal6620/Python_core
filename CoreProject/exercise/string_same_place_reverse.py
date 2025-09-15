4d58799dc2bcea30b2dd553a83d2e83143d82f13
VIshal6620 <vishalvish7099@gmail.com>
2025-09-15 18:30:37 +0530

    15-09-2025

--- CoreProject/exercise/string_same_place_reverse.py
+++ CoreProject/exercise/string_same_place_reverse.py
@@ -1,16 +1,5 @@
-# name = "vijay dinanath chouhan"
-# words = name.split()  # ['vijay', 'dinanath', 'chouhan']
-#
-# for word in words:
-#     reversed_word = ""
-#     for char in word:
-#         reversed_word = char + reversed_word
-#     print(reversed_word, end=" ")
-
-
-
-name = "vishal vishwakarma"
-words = name.split()
+name = "vijay dinanath chouhan"
+words = name.split()  # ['vijay', 'dinanath', 'chouhan']
 
 for word in words:
     reversed_word = ""
@@ -20,11 +9,6 @@ for word in words:
 
 
 
-# name = "vishal"
-# words = name.split()
-#
-# for word in words:
-#     reversed_word = ""
-#     for char in word:
-#         reversed_word = char + reversed_word
-#     print(reversed_word, end="")
+
+
+
