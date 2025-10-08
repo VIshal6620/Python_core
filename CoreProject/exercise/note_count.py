3118f2789d2af32fbc171f43671cee79b2b949e2
VIshal6620 <vishalvish7099@gmail.com>
2025-10-08 17:27:53 +0530

    08-10-2025

--- CoreProject/exercise/note_count.py
+++ CoreProject/exercise/note_count.py
@@ -9,3 +9,40 @@ for i in notes:
     if count > 0:
      print(i, " = ", count)
     amount = amount % i
+
+
+notes = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+amount = 13276
+
+count = 0
+
+for i in notes:
+    count = amount // i
+    if count > 0:
+     print(i, " = ", count)
+    amount = amount % i
+
+    notes = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+    amount = 13276
+
+    count = 0
+
+    for i in notes:
+        count = amount // i
+        if count > 0:
+            print(i, " = ", count)
+        amount = amount % i
+
+        notes = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+        amount = 13276
+
+        count = 0
+
+        for i in notes:
+            count = amount // i
+            if count > 0:
+                print(i, " = ", count)
+            amount = amount % i
